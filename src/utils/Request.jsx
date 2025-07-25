ffe89662f59a75ed2eca8fee5e54f3f6db483041
Aqib ahmad khan <aqibkhan0396@gmail.com>
2025-07-25 20:52:40 +0530

    adding

--- src/utils/Request.jsx
+++ src/utils/Request.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const BASE_URL = "https://todo-olbc.onrender.com/api";
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://kanbanboardbackend.vercel.app/api";
 export const request = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
